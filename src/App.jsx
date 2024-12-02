3fd16c3e313cf704f2e6a5377cf240e8b27cfb5b
Vansh Vasishtha <vansh19092002@gmail.com>
2024-12-02 11:03:17 +0530

    Deployment bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
-import SignIn from "./pages/SignIn";
+import SignIn from "./pages/Signin";
 import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
 import Checkout from "./pages/Checkout";
