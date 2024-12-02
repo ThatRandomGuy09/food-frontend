9a4859d6c5642bf00362a94597f051164ff10ea8
Vansh Vasishtha <vansh19092002@gmail.com>
2024-12-02 11:14:17 +0530

    bug fix

--- src/components/Auth/SignIn.jsx
+++ src/components/Auth/SignIn.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./SignIn.css";
+import "./Signin.css";
 import { signin } from "../../utils/api";
 
 const SignIn = () => {
